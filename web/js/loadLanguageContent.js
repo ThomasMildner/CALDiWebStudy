acf4fab379f43481fabd83f40639d119b3e95cae
ThomasMildner <thomas.s.mildner@gmail.com>
2022-09-27 08:57:16 +0200

    added french language

--- web/js/loadLanguageContent.js
+++ web/js/loadLanguageContent.js
@@ -6,6 +6,7 @@ const englishText = document.querySelector('#english');
 const arabicText = document.querySelector('#arabic');
 const bulgarianText = document.querySelector('#bulgarian');
 const dariText = document.querySelector('#dari');
+const frenchText = document.querySelector('#french');
 const italianText = document.querySelector('#italian');
 const polishText = document.querySelector('#polish');
 const romanianText = document.querySelector('#romanian');
@@ -21,6 +22,7 @@ const Language =  {
     ARABIC: 'Arabic',
     BULGARIAN: 'Bulgarian',
     DARI: 'Dari',
+    FRENCH: 'French',
     ITALIAN: 'Italian',
     POLISH: 'Polish',
     ROMANIAN: 'Romanian',
@@ -149,6 +151,44 @@ const selectAndDisplayLanguage = (language) => {
         case Language.DARI:
             innerLetter.innerHTML= `
                 <h2>فارسی دری</h2>
+                <h1>Europaweite Studie der Weltgesundheitsorganisation (WHO): Wie entwickeln sich Kinder heute?</h1>
+                <hr>
+                <p>Liebe Eltern,<br>
+                in Bremen werden gerade viele Kinder untersucht. Es handelt sich dabei um eine europaweite Studie. Die Ergebnisse erhält die Weltgesundheitsorganisation (WHO). Sie möchte erfahren, wie Kinder sich heute entwickeln. Am Ende sollen Programme erstellt werden, die die Gesundheit der Kinder verbessern. Die Studie ist für Kinder der zweiten und dritten Klasse.
+                <br>
+                Das wird gemessen: Gewicht, Körpergröße, Hüftumfang und Taillenumfang.
+                <br>
+                Ihre Kinder werden lediglich vermessen. So erfahren wir, wie sich Kinder im Durchschnitt entwickeln. Die Untersuchung dauert ungefähr 5 Minuten. Jungen und Mädchen werden getrennt untersucht. Die Kinder sollten leichte Sportkleidung tragen. So können die Fachkräfte besser messen. Die Expertinnen und Experten sind besonders geschult im Umgang mit Kindern.
+                <br>
+                Im Fragebogen sind Fragen dazu, wie Sie zuhause leben und wo Sie geboren sind. Die Daten speichern wir nicht zusammen mit dem Namen des Kindes. Niemand kann erfahren, wie groß oder schwer Ihr Kind ist. Alle Gesetze zum Datenschutz werden beachtet.
+                <br>
+                Die Umfrage wird durchgeführt vom „Leibniz-Institut für Präventionsforschung und Epidemiologie“. In Bremen wurde sie von der Senatorin für „Gesundheit, Frauen und Verbraucherschutz“ und der Senatorin für „Wissenschaft und Häfen“ in Auftrag gegeben.
+                <br>
+                Unterstützen Sie uns. Helfen Sie, die Gesundheit von Kindern zu verbessern. Das können Sie tun, indem Ihr Kind an unserer Studie teilnimmt. Die Teilnahme ist freiwillig. Wenn Sie nicht teilnehmen möchten, hat das keine Nachteile für Sie oder Ihr Kind.
+                <br>
+                Füllen Sie bitte diese beiden Dokumente aus:
+                <br><br>
+                •\tEinverständniserklärung
+                <br>
+                •\tFragebogen.
+                <br><br>
+                Ihre Schule benötigt die ausgefüllten Bögen innerhalb einer Woche zurück.
+                <br>
+                Wenn Sie Fragen haben, rufen Sie uns gern unter <b>0421 218 56830</b> an <br>oder schreiben Sie uns eine E-Mail an <a href="mailto:proxi-at-mail.co"><b>proxi-at-mail.co</b></a>
+                <br><br>
+                Mit freundlichen Grüßen
+                <br>
+                <img src="img/signature-ahrens.png">
+                <br>
+                Prof. Dr. Wolfgang Ahrens
+                <br>
+                </p>
+                `
+            console.log('Selected language: Dari');
+            break;
+        case Language.FRENCH:
+            innerLetter.innerHTML= `
+                <h2>Français</h2>
                 <h1>Europaweite Studie der Weltgesundheitsorganisation (WHO): Wie entwickeln sich Kinder heute?</h1>
                 <hr>
                 <p>Liebe Eltern,<br>
@@ -474,6 +514,7 @@ englishText.addEventListener('click', ()=> selectAndDisplayLanguage(Language.ENG
 arabicText.addEventListener('click', ()=> selectAndDisplayLanguage(Language.ARABIC));
 bulgarianText.addEventListener('click', ()=> selectAndDisplayLanguage(Language.BULGARIAN));
 dariText.addEventListener('click', ()=> selectAndDisplayLanguage(Language.DARI));
+frenchText.addEventListener('click', ()=> selectAndDisplayLanguage(Language.FRENCH));
 italianText.addEventListener('click', ()=> selectAndDisplayLanguage(Language.ITALIAN));
 polishText.addEventListener('click', ()=> selectAndDisplayLanguage(Language.POLISH));
 romanianText.addEventListener('click', ()=> selectAndDisplayLanguage(Language.ROMANIAN));
